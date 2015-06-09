24e49cb8e854b4956b7a3a6f35bfbc2be8fbf092
Maksimov Sergey <konjoot@gmail.com>
2015-06-09 09:12:20 +0300

    fix in routes_test

--- src/treacy/routes_test.go
+++ src/treacy/routes_test.go
@@ -19,7 +19,7 @@ var _ = Describe("App", func() {
 
 	Describe("Routes", func() {
 		It("POST /boards", func() {
-			Expect(engine).To(Handle("POST").On("/boards").By(BoardsCreator))
+			Expect(engine).To(Handle("POST").On("/boards").By(Creator))
 		})
 	})
 })
