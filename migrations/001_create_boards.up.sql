f0f12bd89dd7c6fdc5bf4b720415b1892caf1057
konjoot <konjoot@gmail.com>
2015-06-13 18:02:19 +0300

    shortify description field in boards

--- migrations/001_create_boards.up.sql
+++ migrations/001_create_boards.up.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS boards;
 CREATE TABLE boards (
   id           serial       NOT NULL,
   name         varchar(255) NOT NULL,
-  description  text         NOT NULL,
+  desc         text         NOT NULL,
   created_at   timestamp    DEFAULT (now() at time zone 'utc'),
   updated_at   timestamp    DEFAULT (now() at time zone 'utc'),
   CONSTRAINT boards_pkey PRIMARY KEY(id)
